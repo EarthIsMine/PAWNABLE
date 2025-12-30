26aac09972e33ffe72f6ae231ba4e7c8b738f5c3
Yeonhan Jung <yt4307@gmail.com>
2025-12-30 16:59:20 +0900

    chore: next.config.ts 수정

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,12 +5,6 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   turbopack: {},
-  allowedDevOrigins: [
-    "http://yt4307.mooo.com:3000",
-    "http://localhost:3000",
-    "http://192.168.0.44:3000",
-    "http://192.168.0.99:3000",
-  ],
 };
 
 export default withNextIntl(nextConfig);
